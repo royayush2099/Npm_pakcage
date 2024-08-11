fb2468a152851983707d1244cb4b2c6b9d71e248
Ayushroy <royayush2099@gmail.com>
2024-08-11 12:28:03 +0530

    pushing

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+    Hi! I'm Ayush Roy.
+
+    - GitHub: https://github.com/royayush2099
+    - LinkedIn: https://www.linkedin.com/in/ayush-roy-277a5122a/
+    - Twitter: https://twitter.com/theayushroy2003
+  
+
+    Thanks for running this command!
+`);
